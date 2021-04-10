5fb03211d8f0fa12c83f31b22ef8019a954129b1
Mukulbaid63 <mukulbaid63@gmail.com>
2021-04-11 01:26:41 +0545

    FRONTEND ADDED

---
+++ index.js
@@ -0,0 +1,49 @@
+// const socket = new WebSocket("ws://localhost:8080/");
+const socket = new WebSocket("wss://otmchatroom.herokuapp.com/");
+
+let currentDayOfMonth = 0;
+let currentMonth = 0;
+let currentYear = 0;
+let time = 0;
+let dateString = "";
+
+document.querySelector("button").addEventListener("click", function () {
+  const currentDate = new Date();
+
+  currentDayOfMonth = currentDate.getDate();
+  currentMonth = currentDate.getMonth();
+  currentYear = currentDate.getFullYear();
+  time = currentDate.toLocaleTimeString();
+  dateString =
+    time +
+    " " +
+    currentDayOfMonth +
+    "-" +
+    (currentMonth + 1) +
+    "-" +
+    currentYear;
+  const message = document.querySelector("#util-1 #chat-input").value;
+  const name = document.querySelector("#util-1 #name-input").value;
+  if (name === "Select your Username") alert("Select your Username!!!");
+  else if (message === "") alert("Please enter a message!!!");
+  else {
+    let data = [];
+    data.push(name);
+    data.push(message);
+    data.push(dateString);
+    socket.send(`${data}`);
+  }
+});
+
+socket.addEventListener("message", function (event) {
+  document.querySelector("#chat").innerHTML +=
+    `<span><span class="name">${event.data.substring(
+      0,
+      5
+    )}</span>: <span class="message">${event.data.substring(
+      6,
+      event.data.length - 21
+    )}</span> <cite>${event.data.substring(
+      event.data.length - 20
+    )}</cite></span>` + "<br/>";
+});
